cb61977124363eca72a43ec0bde7bd8efb27736b
Li Tongxin <tomboy17@163.com>
2017-03-29 16:55:26 +0800

    Show Avengers list, use ngFor and hero model.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Hero} from './hero';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Tour of  The Avengers';
+  heroes = [
+    new Hero (1, 'Capital American'),
+    new Hero (2, 'Iron Man'),
+    new Hero(3, 'Black Widow')
+  ];
+  myHero = this.heroes[0];
 }
